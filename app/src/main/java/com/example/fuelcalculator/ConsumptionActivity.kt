5aea93e66a164ad2d48f7aec99b8f62be790a847
renanborges <renan.knx@gmail.com>
2025-03-31 21:30:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fuelcalculator/ConsumptionActivity.kt
@@ -0,0 +1,20 @@
+package com.example.fuelcalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConsumptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_consumption)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fuelcalculator/FuelActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fuelcalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class FuelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fuel)
+
+        val edtFuel = findViewById<TextInputEditText>(R.id.edt_fuel)
+        val btnNext = findViewById<Button>(R.id.btn_next)
+
+        btnNext.setOnClickListener {
+
+            val fuel = edtFuel.text
+            println("Renan " + fuel)
+            val intent = Intent(this, ConsumptionActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
