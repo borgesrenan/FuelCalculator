410fd2b696ea0d78dbd76edb871fe7e9a2526e9e
renanborges <renan.knx@gmail.com>
2025-04-02 00:22:59 +0100

    Last adjustments, comments included

--- app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class DistanceActivity : AppCompatActivity() {
@@ -16,25 +17,25 @@ class DistanceActivity : AppCompatActivity() {
         setContentView(R.layout.activity_distance)
 
         val fuelValue = intent.getFloatExtra("FUEL_VALUE", 0f)
-        val consump = intent.getFloatExtra("CONSUMPTION_VALUE",0f)
+        val consump = intent.getFloatExtra("CONSUMPTION_VALUE", 0f)
 
         val edtDistance = findViewById<TextInputEditText>(R.id.edt_distance)
         val btnCalc = findViewById<Button>(R.id.btn_calc)
 
         btnCalc.setOnClickListener {
+
+            //If its null, field is required
+            val distText = edtDistance.text?.toString() ?: ""
+
+            if (distText.isBlank()) {
+                Snackbar.make(edtDistance, "This field is required.", Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
             val distanceText: Float = edtDistance.text.toString().toFloat()
-
-
-            // Calcula o custo total
             val totalCost = (distanceText / consump) * fuelValue
 
-
-            println("Fuel => " + fuelValue)
-            println("Consump => " + consump)
-            println("Distance => " + distanceText)
-            println("Result => " + totalCost)
-
-            // Envia os dados para a tela de resultado
+            //Sending all the results to the last activity (Result Activity)
             val intent = Intent(this, ResulActivity::class.java)
             intent.putExtra("DISTANCE_VALUE", distanceText)
             intent.putExtra("CONSUMPTION_VALUE", consump)
